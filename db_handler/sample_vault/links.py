eea6cea1f6c76d9565ee5310ef8610d5742b92ab
anuj0456 <anuj0456@gmail.com>
2025-01-12 22:23:26 +0530

    sample_vault folder added so that contributors can run in local

---
+++ db_handler/sample_vault/links.py
@@ -0,0 +1,18 @@
+rss_feed = ["https://machinelearningmastery.com/blog/feed/",
+            #### add more links here
+            ]
+
+sites = {
+    "gh_url": "https://api.github.com",
+    "ph_url": "https://api.producthunt.com/v2/api/graphql",
+    "ph_site_url": "https://www.producthunt.com",
+    "hf_base_url": "https://huggingface.co",
+    "hf_papers_url": "https://huggingface.co/papers?date=2024-12-20",
+    "hf_board_url": "https://huggingface.co/collections/open-llm-leaderboard",
+    "gh_daily_url": "https://github.com/trending/python?since=daily&spoken_language_code=en",
+    "gh_weekly_url": "https://github.com/trending/python?since=weekly&spoken_language_code=en",
+    "arxiv_url": "http://export.arxiv.org/api/query?",
+    "events_url": ["https://conferencealerts.co.in/artificial-intelligence",
+                  "https://aideadlin.es/?sub=ML,CV,CG,NLP,RO,SP,DM,AP,KR,HCI"],
+    "events_feed":"https://aiml.events/feed/rss/"
+}
