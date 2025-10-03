5d864d2ae1743810186f8d329b992e2dd1ec070a
anuj0456 <anuj0456@gmail.com>
2025-10-03 21:06:48 +0530

    requirements.txt updated

--- launch.py
+++ launch.py
@@ -9,5 +9,5 @@ app.register_blueprint(bp)
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5001))
     app.run(host="0.0.0.0", port=port, debug=True)
